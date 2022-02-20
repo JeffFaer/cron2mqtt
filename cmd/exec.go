e08f210eb92298517b549640d103483bdaade719
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2022-02-19 22:25:56 -0700

    Use the original args[0] instead of the execution result's args[0], which is now always $SHELL.

--- cmd/exec.go
+++ cmd/exec.go
@@ -19,7 +19,8 @@ func init() {
 		Args:  cobra.MinimumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			id := args[0]
-			res := run(args[1:])
+			args = args[1:]
+			res := run(args)
 
 			if res.Err != nil {
 				fmt.Fprintln(os.Stderr, res.Err)
@@ -31,7 +32,7 @@ func init() {
 			if c, err := loadConfig(); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			} else {
-				if err := publish(id, c, res); err != nil {
+				if err := publish(id, c, args, res); err != nil {
 					fmt.Fprintf(os.Stderr, "Could not publish to MQTT: %s\n", err)
 				}
 			}
@@ -49,14 +50,14 @@ func run(args []string) exec.Result {
 	return exec.Run(sh, "-c", strings.Join(args, " "))
 }
 
-func publish(id string, conf mqtt.Config, res exec.Result) error {
+func publish(id string, conf mqtt.Config, args []string, res exec.Result) error {
 	c, err := mqtt.NewClient(conf)
 	if err != nil {
 		return fmt.Errorf("could not initialize MQTT: %w", err)
 	}
 	defer c.Close(250)
 
-	cj, err := hass.NewCronJob(c, id, res.Args[0])
+	cj, err := hass.NewCronJob(c, id, args[0])
 	if err != nil {
 		return fmt.Errorf("could not create hass.CronJob: %w", err)
 	}
